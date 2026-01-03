fefdfe2b0277f4392805409cde3896adb23d6ee1
Kiran Dahal <dahalkiran926@gmail.com>
2026-01-03 15:05:53 +0545

    shaders implemented

--- graphics_utils.py
+++ graphics_utils.py
@@ -1,5 +1,64 @@
 from OpenGL.GL import *
 
+# shader source code 
+VERTEX_SHADER_SOURCE = """
+layout (location = 0) in vec3 aPos;
+uniform mat4 MVP; // Model-View-Projection Matrix
+
+void main() {
+    // Transform the 3D Lorenz point to screen space
+    gl_Position = MVP * vec4(aPos, 1.0);
+}
+"""
+
+FRAGMENT_SHADER_SOURCE = """
+#version 330 core
+out vec4 FragColor;
+uniform vec3 uColor;
+
+void main() {
+    // Set the color of the line
+    FragColor = vec4(uColor, 1.0);
+}
+"""
+
+def create_shader_program():
+    #1. compile vertex shader
+    vs = glCreateShader(GL_VERTEX_SHADER)
+    glShaderSource(vs, VERTEX_SHADER_SOURCE)
+    glCompileShader(vs)
+
+    if not glGetShaderiv(vs, GL_COMPILE_STATUS):
+        raise RuntimeError(f"Vertex Shader Error: {glGetShaderInfoLog(vs).decode()}")
+
+    # 2. compile fragment shader
+    fs = glCreateShader(GL_FRAGMENT_SHADER)
+    glShaderSource(fs, FRAGMENT_SHADER_SOURCE)
+    glCompileShader(fs)
+
+
+    if not glGetShaderiv(fs, GL_COMPILE_STATUS):
+        raise RuntimeError(f"Fragment Shader Error: {glGetShaderInfoLog(fs).decode()}")
+    
+
+    # 3. Linking shaders into a program
+    program = glCreateProgram()
+    glAttachShader(program, vs)
+    glAttachShader(program, fs)
+    glLinkProgram(program)
+
+
+    if not glGetProgramiv(program, GL_LINK_STATUS):
+        raise RuntimeError(f"Shader linking error: {glGetProgramInfoLog(program).decode()}")
+        
+
+    glDeleteShader(vs)
+    glDeleteShader(fs)
+
+    return program
+
+
+
 def create_trajectory_buffer(max_points):
     """
     Allocates memory on the GPU for the trajectory.
